dd3449dc36c5555f594a066a3a4ae9a8600c3403
mahafujahamed <mahafujahamed068@gmail.com>
2024-02-02 02:41:42 +0600

    adds some problems

--- python-impliment/looping.py
+++ python-impliment/looping.py
@@ -4,9 +4,19 @@
 #     x += 1
 # print("o------------o------------o")
 
-i = 1
+# i = 1
 
-while i <= 6:
-    print('*' * i)
-    i += 1
-print("Wow!")
+# while i <= 6:
+#     print('*' * i)
+#     i += 1
+# print("Wow!")
+
+# largest number in a list
+
+numbers = [5, 32, 56, 4, 5, 6, 20]
+max = numbers[0]
+
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
--- python-impliment/projects/cargame.py
+++ python-impliment/projects/cargame.py
@@ -1,15 +1,24 @@
 command = ""
+started = False
 while True:
     command = input(">> ").lower()
     if command == "start":
-        print("Car started...")
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car started...")
     elif command == "stop":
-        print("Car is Stopped...")
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("Car is  Stopped...")
     elif command == "help":
         print("""
-              start - to start the car
-              stop - to stop the car
-              quit - to quit
+start - to start the car
+stop - to stop the car
+quit - to quit
         """)
     elif command == "quit":
         break
