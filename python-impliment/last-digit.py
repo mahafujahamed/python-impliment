1d9a52cb754145ca4a0064d70678753d9dfa7120
mahafujahamed <mahafujahamed068@gmail.com>
2024-02-08 00:07:03 +0600

    added last digit of integer problem

---
+++ python-impliment/last-digit.py
@@ -0,0 +1,8 @@
+number = int(input("Enter an integer number: "))
+last_digit = number % 10
+print(f"The last digit of {number} is {last_digit}.")
+# Output:
+# Enter an integer number: 123456
+# The last digit of 123456 is 6.
+# Enter an integer number: 123456789
+# The last digit of 123456789 is 9.
