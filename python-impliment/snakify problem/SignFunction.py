b8f4326e17c25ec472d052c3c4cd08854797eb7b
mahafujahamed <mahafujahamed068@gmail.com>
2024-02-08 20:08:41 +0600

    added if-else problem

---
+++ python-impliment/snakify problem/SignFunction.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number: "))
+if x > 0:
+    print(1)
+elif x < 0:
+    print(-1)
+else:
+    print(0)
---
+++ python-impliment/snakify problem/twoNumbers.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a < b and b > a:
+    print(a)
+else:
+    print(b)
