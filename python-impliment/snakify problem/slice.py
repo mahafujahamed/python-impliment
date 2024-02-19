603facc9cbcaad90093e1ef5940eae0e688e502b
mahafujahamed <mahafujahamed068@gmail.com>
2024-02-19 19:30:31 +0600

    added string slice problem

---
+++ python-impliment/snakify problem/slice.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print(name[2]) # 4th character
+print(name[-2]) # 3rd to last character
+print(name[0:5]) # first 5 characters
+print(name[0:len(name) - 2]) # all characters
+print(name[0:len(name):2]) # every 2nd character
+print(name[1:len(name):2]) # every 2nd character starting from 2nd character
+print(name[::-1]) # reverse the string
+print(name[::-2]) # reverse the string and every 2nd character
+print(len(name)) # length of the string
+# print(name[100]) # IndexError: string index out of range
